18bc95ad7ed2e6380f724c2a69badd399def9286
Rojor3d <mrogeliscaballero@gmail.com>
2025-10-08 14:09:59 -0500

    database

--- src/main/java/DataBaseConnection.java
+++ src/main/java/DataBaseConnection.java
@@ -1,6 +1,16 @@
 package src.main.java;
 
-public class DataBaseConnection 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection 
 {
-    
+    private static final String URL = "jdbc:sqlserver://<your-server>.database.windows.net:1433;database=<your-db>";
+    private static final String USER = "<your-username>";
+    private static final String PASSWORD = "<your-password>";
+    public static Connection getConnection() throws SQLException 
+    {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
 }
